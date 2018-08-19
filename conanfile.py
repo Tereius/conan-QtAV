797e1397592344050daada3ade6ea4c1eb9465e5
Björn Stresing <bjoern.stresing@gmx.de>
2018-08-19 15:43:01 +0200

    Support for android

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
 from shutil import copyfile
-from conans import ConanFile, VisualStudioBuildEnvironment, MSBuild, tools
+from conans import ConanFile, VisualStudioBuildEnvironment, AutoToolsBuildEnvironment, MSBuild, tools
 
 
 class QtavConan(ConanFile):
@@ -11,14 +14,14 @@ class QtavConan(ConanFile):
     description = "QtAV is a multimedia playback library based on Qt and FFmpeg"
     author = "Bjoern Stresing"
     homepage = "https://www.qtav.org/"
-    requires = "Qt/5.11.1@bincrafters/stable", "ffmpeg/4.0@bincrafters/stable"
-    settings = "os", "compiler", "build_type", "arch"
+    requires = "Qt/5.11.1@tereius/stable", "ffmpeg/4.0@tereius/stable"
+    settings = "os", "compiler", "build_type", "arch", "os_build", "arch_build"
     options = {"shared": [True, False]}
     default_options = ("shared=True",
                         "Qt:shared=True",
                         "Qt:GUI=True",
                         "Qt:widgets=True",
-                        "Qt:openssl=True",
+                        "Qt:openssl=False",
                         "Qt:qtbase=True",
                         "Qt:qtsvg=True",
                         "Qt:qtdeclarative=True",
@@ -29,50 +32,69 @@ class QtavConan(ConanFile):
                         "Qt:qtgraphicaleffects=True",
                         "Qt:qtquickcontrols=True",
                         "Qt:qtquickcontrols2=True",
-                        "ffmpeg:shared=True",
-                        "ffmpeg:lzma=False",
-                        "ffmpeg:bzlib=False",
-                        "ffmpeg:iconv=False",
-                        "ffmpeg:freetype=False",
-                        "ffmpeg:openjpeg=False",
-                        "ffmpeg:openh264=False",
-                        "ffmpeg:opus=False",
-                        "ffmpeg:vorbis=False",
-                        "ffmpeg:zmq=False",
-                        "ffmpeg:sdl2=False",
-                        "ffmpeg:x264=False",
-                        "ffmpeg:x265=False",
-                        "ffmpeg:vpx=False",
-                        "ffmpeg:mp3lame=False",
-                        "ffmpeg:fdk_aac=False")
-    #generators = "cmake"
-    build_policy = "missing"
+                        "ffmpeg:shared=True")
 
     def source(self):
         git = tools.Git(folder="QtAV")
         git.clone("https://github.com/wang-bin/QtAV.git")
         git.checkout("34afa14316c2052bcef2822e82b32c11e0939e54")
+        self.run("git submodule init && git submodule update", cwd="QtAV")
+
+    def build_requirements(self):
+        if self.settings.os == 'Android':
+            self.build_requires("android-ndk/r17b@tereius/stable")
+            if self.settings.os_build == 'Windows':
+                self.build_requires("msys2/20161025@tereius/stable")
+
+    def configure(self):
+        if self.settings.os == 'Android' and self.settings.os_build == 'Windows':
+            self.options["msys2"].provideMinGW = True
+            self.options["Qt"].qtandroidextras = True
 
     def build(self):
-        env_build = VisualStudioBuildEnvironment(self)
-        with tools.environment_append(env_build.vars):
-            vcvars = tools.vcvars_command(self.settings)
-            self.run('%s && qmake -r -tp vc "LIBS+=%s" "INCLUDEPATH+=%s" QtAV.pro' % (vcvars, ' -L'+ ' -L'.join(env_build.lib_paths), ' '.join(env_build.include_paths)), cwd="QtAV")
-            ms_env = MSBuild(self)
-            ms_env.build(project_file="QtAV/QtAV.sln", targets=["QtAV", "QtAVWidgets"], upgrade_project=False) # Missing dependency information between targets
-            ms_env.build(project_file="QtAV/QtAV.sln", upgrade_project=False)
+
+        build_type = "debug" if self.settings.build_type == "Debug" else "release"
+
+        if self.settings.os == "Windows":
+            env_build = VisualStudioBuildEnvironment(self)
+            with tools.environment_append(env_build.vars):
+                vcvars = tools.vcvars_command(self.settings)
+                tools.replace_in_file("QtAV/.qmake.conf", "QTAV_MAJOR_VERSION = 1", "QTAV_MAJOR_VERSION = 1\nLIBS += %s\nINCLUDEPATH += %s\nCONFIG += %s\n" % (' -L'+ ' -L'.join(env_build.lib_paths), ' '.join(env_build.include_paths), build_type))
+                self.run('%s && qmake -r -tp vc QtAV.pro' % (vcvars), cwd="QtAV")
+                ms_env = MSBuild(self)
+                ms_env.build(project_file="QtAV/QtAV.sln", targets=["QtAV", "QtAVWidgets"], upgrade_project=False) # Missing dependency information between targets
+                ms_env.build(project_file="QtAV/QtAV.sln", upgrade_project=False)
+
+        elif self.settings.os == "Android" and self.settings.os_build == "Windows":
+                autotools = AutoToolsBuildEnvironment(self, win_bash=True)
+                with tools.environment_append(autotools.vars):
+                    tools.replace_in_file("QtAV/.qmake.conf", "QTAV_MAJOR_VERSION = 1", "QTAV_MAJOR_VERSION = 1\nLIBS += %s\nINCLUDEPATH += %s\nCONFIG += %s\n" % (' -L'+ ' -L'.join(autotools.library_paths), ' '.join(autotools.include_paths), build_type))
+                    self.run('qmake QtAV.pro', win_bash=True, cwd="QtAV")
+                    self.run("make", win_bash=True, cwd="QtAV")
 
     def package(self):
 
         arch = str(self.settings.arch)
-        self.copy("QtAV/lib_win_" + arch + "/*Qt*AV*.lib", dst="lib", keep_path=False)
+        if self.settings.os == "Windows":
+            self.copy("QtAV/lib_win_" + arch + "/*Qt*AV*.lib", dst="lib", keep_path=False)
+        elif self.settings.os == "Android":
+            self.copy("QtAV/lib_android_" + "/*Qt*AV*.so", dst="lib", keep_path=False)
+
         #self.copy("QtAV/lib_win_" + arch + "/QtAV1.lib", dst="lib/Qt5AV.lib", keep_path=False)
         #self.copy("QtAV/lib_win_" + arch + "/QtAVd1.lib", dst="lib/Qt5AVd.lib", keep_path=False)
-        self.copy("QtAV/lib_win_" + arch + "/*QmlAV*.lib", dst="lib/qml/QtAV", keep_path=False)
+
+        if self.settings.os == "Windows":
+            self.copy("QtAV/lib_win_" + arch + "/*QmlAV*.lib", dst="lib/qml/QtAV", keep_path=False)
+        elif self.settings.os == "Android":
+            self.copy("QtAV/lib_android_" + "/*QmlAV*.so", dst="lib/qml/QtAV", keep_path=False)
+
         #self.copy("QtAV/lib_win_" + arch + "/QtAVWidgets1.lib", dst="lib/Qt5AVWidgets.lib", keep_path=False)
         #self.copy("QtAV/lib_win_" + arch + "/QtAVWidgetsd1.lib", dst="lib/Qt5AVWidgetsd.lib", keep_path=False)
-        self.copy("QtAV/bin/Qt*AV*.dll", dst="bin", keep_path=False)
-        self.copy("QtAV/bin/*QmlAV*.dll", dst="lib/qml/QtAV", keep_path=False)
+
+        if self.settings.os == "Windows":
+            self.copy("QtAV/bin/Qt*AV*.dll", dst="bin", keep_path=False)
+            self.copy("QtAV/bin/*QmlAV*.dll", dst="lib/qml/QtAV", keep_path=False)
+
         self.copy("QtAV/src/QtAV/*.h", dst="include/QtAV", keep_path=False)
         copyfile(self.build_folder + "/QtAV/src/QtAV/QtAV", self.package_folder + "/include/QtAV/QtAV") # Not a folder
         self.copy("QtAV/widgets/QtAVWidgets/*.h", dst="include/QtAVWidgets", keep_path=False)
@@ -82,13 +104,4 @@ class QtavConan(ConanFile):
         self.copy("QtAV/qml/plugins.qmltypes", dst="lib/qml/QtAV", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.includedirs = ['include']
-        self.cpp_info.libs = []
-        self.cpp_info.libdirs = ['lib']
-        self.cpp_info.builddirs = ['cmake']
-        self.cpp_info.bindirs = ['bin']
-        self.cpp_info.defines = []
-        self.cpp_info.cflags = []
-        self.cpp_info.cppflags = []
-        self.cpp_info.sharedlinkflags = []
-        self.cpp_info.exelinkflags = []
+        self.env_info.QML2_IMPORT_PATH.append(os.path.join(self.package_folder, "lib/qml/QtAV"))
